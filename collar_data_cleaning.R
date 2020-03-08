f515b5058e44ea2d00ad2400b700a4193ecb4307
Geoff Miller <gdevonmiller@gmail.com>
2020-03-08 13:29:53 -0500

    Finished a substantail amount of the data cleaning code for the collar data, will likely need to do more with fuller sets of data. Also, will contact James to ask about filtering out 'bad' gps fixes, etc.

---
+++ collar_data_cleaning.R
@@ -0,0 +1,100 @@
+# Reading in the data
+collars <- read.csv("C:/Users/Geoffrey/Desktop/collar_data.csv", header = T, sep = ",", na.strings = NA)
+
+# I like underscores and all lowercase in headers, so why not fix that?
+names(collars) <- lapply(names(collars), function(v) {
+  if (is.character(v)) return(tolower(v))
+  else return(v)
+})
+
+names(collars) <- gsub("\\.", "_", names(collars))
+
+# Get rid of the NA rows introduced from the Macros in Excel.
+collars <- collars[!is.na(collars$acquisition_time),]
+
+# Get rid of predeployment data
+collars <- collars[!collars$predeployment_data == "Yes",]
+
+#Create a collar identification number column
+collars$collar_id <- 0
+for (i in 1:dim(collars)[1]) {
+    if (is.na(collars$ctn[i])) {
+      collars$collar_id[i] <- NA
+    } else if (collars$ctn[i] == "712851A") {
+      collars$collar_id[i] <- "F1"
+    } else if (collars$ctn[i] == "712852A") {
+      collars$collar_id[i] <- "F2"
+    } else if (collars$ctn[i] == "712853A") {
+      collars$collar_id[i] <- "F3"
+    } else if (collars$ctn[i] == "712857A") {
+      collars$collar_id[i] <- "F7"
+    } else if (collars$ctn[i] == "712859A") {
+      collars$collar_id[i] <- "F8"
+    } else if (collars$ctn[i] == "712879A") {
+      collars$collar_id[i] <- "C1"
+    } else if (collars$ctn[i] == "712880A") {
+      collars$collar_id[i] <- "C2"
+    } else if (collars$ctn[i] == "712881A") {
+      collars$collar_id[i] <- "C3"
+    } else if (collars$ctn[i] == "712882A") {
+      collars$collar_id[i] <- "C4"
+    } else if (collars$ctn[i] == "712883A") {
+      collars$collar_id[i] <- "C5"
+    } else if (collars$ctn[i] == "712884A") {
+      collars$collar_id[i] <- "C6"
+    } else if (collars$ctn[i] == "712885A") {
+      collars$collar_id[i] <- "C7"
+    }
+}
+
+# Datetimes to POSIXct
+collars$acquisition_time <- as.POSIXct(collars$acquisition_time, format = "%Y.%m.%d %H:%M:%S")
+collars$gps_fix_time <- as.POSIXct(collars$gps_fix_time, format = "%Y.%m.%d %H:%M:%S")
+collars$acquisition_start_time <- as.POSIXct(collars$acquisition_start_time, format = "%Y.%m.%d %H:%M:%S")
+collars$receive_time <- as.POSIXct(collars$receive_time, format = "%Y.%m.%d %H:%M:%S")
+
+# Deal with redeployment of collars
+collars$animal_id <- 0
+for (i in 1:dim(collars)[1]) {
+  if (is.na(collars$collar_id[i])) {
+    collars$animal_id[i] <- NA
+  } else if (!is.na(collars$collar_id[i]) & is.na(collars$gps_fix_time[i])) {
+    collars$animal_id[i] <- collars$animal_id[i - 1]
+  }  else if (collars$collar_id[i] == "C1" & collars$gps_fix_time[i] > as.POSIXct("2020-02-03 00:00:00")) {
+    collars$animal_id[i] <- "C11"
+  } else {
+    collars$animal_id[i] <- collars$collar_id[i]
+  }
+}
+
+# If this is direct from satellite, filter out useless columns as well as predeployment and others used for filtering.
+collars <- subset(collars, select = c(acquisition_time,
+                                      acquisition_start_time,
+                                      gps_fix_time,
+                                      gps_fix_attempt,
+                                      gps_latitude,
+                                      gps_longitude,
+                                      gps_utm_zone,
+                                      gps_utm_northing,
+                                      gps_utm_easting,
+                                      gps_altitude,
+                                      gps_horizontal_error,
+                                      gps_horizontal_dilution,
+                                      gps_satellite_bitmap,
+                                      gps_satellite_count,
+                                      gps_navigation_time,
+                                      activity_count,
+                                      temperature,
+                                      satellite_uplink,
+                                      receive_time,
+                                      repetition_count,
+                                      low_voltage,
+                                      mortality,
+                                      iridium_command,
+                                      error,
+                                      ctn,
+                                      collar_id,
+                                      animal_id
+                                      ))
+
+#
