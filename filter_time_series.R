9e0ea133aa5329a0cbf65485a165c00858fd84aa
Geoff Miller <gdevonmiller@gmail.com>
2020-04-12 21:55:23 -0500

    Finished functions 'internalizer' and 'tracker', both are now annotated and reasonably user-friendly

--- filter_time_series.R
+++ filter_time_series.R
@@ -1,347 +1,99 @@
-# Filter out bursts
-function(x) {
-for (i in 1:as.numeric(dim(collars))) {
-  if (collars$animal_id[i + 1] != collars$animal_id[i] | as.numeric(dim(collars)) == i) {
-    collars$difftime[i] <- NA
-  } else if (collars$animal_id[i + 1] == collars$animal_id[i]) {
-    collars$difftime[i] <- difftime(collars$acquisition_time[i + 1], collars$acquisition_time[i], units = "hours")
-  }
-}
-for (i in 1:as.numeric(dim(collars))) {
-  if (collars$difftime[i] < 4) {
-    j = i
-    while (collars$difftime[j+1] < 1 | is.na(collars$difftime[j+1])) {
-      collars <- collars[-(j+1),]
-    }
-  }
-}
-if (collars$difftime[1] < 4) {
-  collars <- collars[!1,]
-}
-collars$difftime <- NULL
-
-# Filter in bursts
-for (i in 1:as.numeric(dim(collars))) {
-  if (collars$animal_id[i + 1] != collars$animal_id[i] | as.numeric(dim(collars)) == i) {
-    collars$difftime[i] <- NA
-  } else if (collars$animal_id[i + 1] == collars$animal_id[i]) {
-    collars$difftime[i] <- difftime(collars$acquisition_time[i + 1], collars$acquisition_time[i], units = "hours")
-  }
-}
-k = 0
-for (i in 1:dim(collars)) {
-  if (is.na(collars$difftime[i])) {
-    if (is.null(collars$difftime[i + 1])) {
-      collars$burst_id[i] <- k
-      break
-    } else if (collars$difftime[i+1] > 1) {
-      j = i  
-      while (collars$difftime[j+1] > 1 | is.na(collars$difftime[j+1])) {
-        collars <- collars[-(j+1),]
-      }
-      collars$burst_id[j] <- k
-      k = k + 1
-    } else {
-      collars$burst_id[i] <- k
-    }
-  } else if (collars$difftime[i] > 1) {
-    j = i
-    while (collars$difftime[j+1] > 1 | is.na(collars$difftime[j+1])) {
-      collars <- collars[-(j+1),]
-    }
-    collars$burst_id[j] <- k
-    k = k + 1
-  } else {
-    collars$burst_id[i] <- k
-  }
-}
-if (collars$difftime[1] > 1) {
-  collars <- collars[-1,]
-}
-collars$difftime <- NULL
-
-
-####################################################################################
-
-regular.bursts <- function(anid, df, t, unit, lower, upper) {
-  # Variable anid is the unique animal identification number
-  # Variable df is the data frame that is being used for the analysis
-  # Variable u is the units that will be used in the difftime function
-  # Variable t is the fix time
-  for (i in 1:dim(df)) {
-    if (df[i + 1, anid] != df[i, anid] | dim(df) == i) {
+##############INTERVALIZER######################################################################################
+################################################################################################################
+intervalizer <- function(df, name, an.id, t, lower, upper, unit) { #############################################
+  ##############################################################################################################
+  ############ Function specifications #########################################################################
+  # Function 'intervalizer' will take time series data and make it into equal interval 
+  # bursts by animal
+  # Variable 'df' is the data frame that is being used for the analysis
+  # Variable 'name' is the name that will be specified for the new table
+  #   defaults to name of input 'df', which will replace input table
+  # Variable 'an.id' is the column containing unique animal identification number
+  # Variable 't' is the column containing the raw time sequence data
+  # Variable 'upper' is the upper limit of acceptable values for the interval
+  # Variable 'lower' is the lower limit of acceptable values for the interval
+  # Variable u is the units that will be used in the difftime function ("hours", "mins")
+  ##############################################################################################################
+  ############ Columns to be created ###########################################################################
+  # Column 'burst_id' will contain unique identifiers for bursts for later analyses
+  ##############################################################################################################
+  ############ Function ########################################################################################
+  
+  for (i in 1:nrow(df)) {
+    if (df[i + 1, an.id] != df[i, an.id] | nrow(df) == i) { 
+      # Inputs an NA where there is a transition between animals
       df[i, "dt"] <- NA
-    } else if (df[i + 1, anid] == df[i, anid]) {
+    } else if (df[i + 1, an.id] == df[i, an.id]) { 
+      # Calculates the time difference between the current point and the next observation
       df[i, "dt"] <- difftime(df[i + 1, t], df[i, t], units = unit)
     }
   }
-  k = 0
-  for (i in 1:dim(df)) {
+  for (i in 1:nrow(df)) {
     if (is.na(df[i, "dt"])) {
-      if (is.null(df[i + 1, "dt"]) | is.na(df[i + 1, "dt"])) {
-        df[i, "burst_id"] <- k
-      } else if (df[i + 1, "dt"] > upper | df[i + 1, "dt"] < lower) {
-        j = i  
-        while (df[j + 1, "dt"] > upper | df[j + 1, "dt"] < lower | is.na(df[j + 1, "dt"])) {
+      # Do nothing if there is an NA
+    } else if (df[i, "dt"] < lower) {
+      # Remove values that are lower than the specified interval
+      j = i
+      while (df[j+1, "dt"] < lower | is.na(df[j+1, "dt"])) { 
+        # This will delete all values lower than 'lower' until something higher than 'lower'
+        # is encountered
+        if (is.na(df[j+2, "dt"])) { 
+          # This takes care of when the end of the table is reached so there is not an 
+          # eternal while
+          df <- df[-(j+2),] # Remove the row two ahead
+          df <- df[-(j+1),] # Remove next row
+          break # Break while
+        } else {
+          # This will be the condition for every situation except the end of the table,
+          # delete following row while condition is met
           df <- df[-(j+1),]
         }
-        df[j, "burst_id"] <- k
-        k = k + 1
+      }
+    }
+  }
+  k = 0 # Set k to 0 for numbering burst id rows
+  for (i in 1:nrow(df)) {
+    # This will filter out higher values while naming bursts
+    if (is.na(df[i, "dt"])) {
+      # NA can signify multiple things in this case
+      if (is.null(df[i + 1, "dt"]) | is.na(df[i + 1, "dt"])) {
+        # Condition for the end of the table so there is no possibility of an eternal while
+        df[i, "burst_id"] <- k
+        break
+      } else if (df[i + 1, "dt"] > upper) {
+        # Remove values higher than upper until condition is no longer met
+        j = i  
+        while (df[j + 1, "dt"] > upper | is.na(df[j + 1, "dt"])) {
+          df <- df[-(j+1),]
+        }
+        df[j, "burst_id"] <- k # Assign burst id number
+        k = k + 1 # Increase for next assignment
       } else {
+        # Assign burst id for all values within range
         df[i, "burst_id"] <- k
       }
-    } else if (df[i, "dt"] > upper | df[i, "dt"] < lower) {
+    } else if (df[i, "dt"] > upper) {
+      # Remove values higher than upper until condition is no longer met
       j = i
-      while (df[j + 1, "dt"] > upper | df[j + 1, "dt"] < lower | is.na(df[j + 1, "dt"])) {
+      while (df[j + 1, "dt"] > upper | is.na(df[j + 1, "dt"])) {
         df <- df[-(j+1),]
       }
       df[j, "burst_id"] <- k
       k = k + 1
     } else {
+      # Assign burst id for all values within range
       df[i, "burst_id"] <- k
     }
   }
   if (df[1, "dt"] > upper | df[1, "dt"] < lower) {
+    # If the first value in the table is not within the interval, remove it
     df <- df[-1,]
   }
+  # Remove the row of time differences
   df[, "dt"] <- NULL
+  # Assign a file to be returned
   newfile <- df
-  return(assign("test", newfile, envir = .GlobalEnv))
-}
-
-fine.bursts <- function(anid, df, t, unit, up) {
-  # Variable anid is the unique animal identification number
-  # Variable df is the data frame that is being used for the analysis
-  # Variable u is the units that will be used in the difftime function
-  # Variable t is the fix time
-  for (i in 1:nrow(df)) {
-    if (df[i + 1, anid] != df[i, anid] | nrow(df) == i) {
-      df[i, "dt"] <- NA
-    } else if (df[i + 1, anid] == df[i, anid]) {
-      df[i, "dt"] <- difftime(df[i + 1, t], df[i, t], units = unit)
-    }
-  }
-  k = 0
-  for (i in 1:nrow(df)) {
-    if (is.na(df[i, "dt"])) {
-      if (is.null(df[i + 1, "dt"]) | is.na(df[i + 1, "dt"])) {
-        df[i, "burst_id"] <- k
-        break
-      } else if (df[i + 1, "dt"] > up) {
-        j = i  
-        while (df[j + 1, "dt"] > up | is.na(df[j + 1, "dt"])) {
-          df <- df[-(j+1),]
-        }
-        df[j, "burst_id"] <- k
-        k = k + 1
-      } else {
-        df[i, "burst_id"] <- k
-      }
-    } else if (df[i, "dt"] > up) {
-      j = i
-      while (df[j + 1, "dt"] > up | is.na(df[j + 1, "dt"])) {
-        df <- df[-(j+1),]
-      }
-      df[j, "burst_id"] <- k
-      k = k + 1
-    } else {
-      df[i, "burst_id"] <- k
-    }
-  }
-  if (df[1, "dt"] > up) {
-    df <- df[-1,]
-  }
-  newfile <- df
-  return(assign("test", newfile, envir = .GlobalEnv))
-}
-
-fine.bursts("animal_id", collars, "acquisition_time", "hours", 7)
-
-large.bursts <- function(anid, df, t, unit, low) {
-  # Variable anid is the unique animal identification number
-  # Variable df is the data frame that is being used for the analysis
-  # Variable u is the units that will be used in the difftime function
-  # Variable t is the fix time
-  for (i in 1:nrow(df)) {
-    if (df[i + 1, anid] != df[i, anid] | nrow(df) == i) {
-      df[i, "dt"] <- NA
-    } else if (df[i + 1, anid] == df[i, anid]) {
-      df[i, "dt"] <- difftime(df[i + 1, t], df[i, t], units = unit)
-    }
-  }
-  k = 0
-  for (i in 1:nrow(df)) {
-    if (is.na(df[i, "dt"])) {
-      if (is.null(df[i + 1, "dt"]) | is.na(df[i + 1, "dt"])) {
-        df[i, "burst_id"] <- k
-        break
-      } else if (df[i + 1, "dt"] < low) {
-        j = i  
-        while (df[j + 1, "dt"] < low | is.na(df[j + 1, "dt"])) {
-          df <- df[-(j+1),]
-        }
-        df[j, "burst_id"] <- k
-        k = k + 1
-      } else {
-        df[i, "burst_id"] <- k
-      }
-    } else if (df[i, "dt"] < low) {
-      j = i
-      while (df[j + 1, "dt"] < low | is.na(df[j + 1, "dt"])) {
-        df <- df[-(j+1),]
-      }
-      df[j, "burst_id"] <- k
-      k = k + 1
-    } else {
-      df[i, "burst_id"] <- k
-    }
-  }
-  if (df[1, "dt"] < low) {
-    df <- df[-1,]
-  }
-  newfile <- df
-  return(assign("test", newfile, envir = .GlobalEnv))
-}
-
-large.bursts("animal_id", collars, "acquisition_time", "hours", 5)
-
-#######################
-
-
-bursts <- function(anid, df, t, unit, low, up) {
-  # Variable anid is the unique animal identification number
-  # Variable df is the data frame that is being used for the analysis
-  # Variable u is the units that will be used in the difftime function
-  # Variable t is the fix time
-  for (i in 1:nrow(df)) {
-    if (df[i + 1, anid] != df[i, anid] | nrow(df) == i) {
-      df[i, "dt"] <- NA
-    } else if (df[i + 1, anid] == df[i, anid]) {
-      df[i, "dt"] <- difftime(df[i + 1, t], df[i, t], units = unit)
-    }
-  }
-  for (i in 1:nrow(df)) {
-    if (is.na(df[i, "dt"])){
-    } else if (df[i, "dt"] < low) {
-      j = i
-      while (df[j + 1, "dt"] < low | is.na(df[j + 1, "dt"])) {
-        df <- df[-(j + 1),]
-      }
-    }
-  }
-  k = 0
-  for (i in 1:nrow(df)) {
-    if (is.na(df[i, "dt"])) {
-      if (is.null(df[i + 1, "dt"]) | is.na(df[i + 1, "dt"])) {
-        df[i, "burst_id"] <- k
-        break
-      } else if (df[i + 1, "dt"] > up) {
-        j = i  
-        while (df[j + 1, "dt"] > up | is.na(df[j + 1, "dt"])) {
-          df <- df[-(j+1),]
-        }
-        df[j, "burst_id"] <- k
-        k = k + 1
-      } else {
-        df[i, "burst_id"] <- k
-      }
-    } else if (df[i, "dt"] > up) {
-      j = i
-      while (df[j + 1, "dt"] > up | is.na(df[j + 1, "dt"])) {
-        df <- df[-(j+1),]
-      }
-      df[j, "burst_id"] <- k
-      k = k + 1
-    } else {
-      df[i, "burst_id"] <- k
-    }
-  }
-  if (df[1, "dt"] > up) {
-    df <- df[-1,]
-  }
-  newfile <- df
-  return(assign("test", newfile, envir = .GlobalEnv))
-}
-
-bursts("animal_id", test, "acquisition_time", "hours", 4, 10)
-
-allalallal
-
-bursts <- function(anid, df, t, unit, low) {
-  # Variable anid is the unique animal identification number
-  # Variable df is the data frame that is being used for the analysis
-  # Variable u is the units that will be used in the difftime function
-  # Variable t is the fix time
-  for (i in 1:nrow(df)) {
-    if (df[i + 1, anid] != df[i, anid] | nrow(df) == i) {
-      df[i, "dt"] <- NA
-    } else if (df[i + 1, anid] == df[i, anid]) {
-      df[i, "dt"] <- difftime(df[i + 1, t], df[i, t], units = unit)
-    }
-  }
-  for (i in 1:nrow(df)) {
-    if (is.na(df[i, "dt"])){
-    } else if (df[i, "dt"] < low) {
-      j = i
-      while (df[j + 1, "dt"] < 1 | is.na(df[j + 1, "dt"])) {
-        df <- df[-(j + 1),]
-      }
-    }
-    if (df[1, "dt"] < low) {
-      df <- df[-1,]
-    }
-  }
-  newfile <- df
-  return(assign("test", newfile, envir = .GlobalEnv))
-}
-
-bursts("animal_id", test, "acquisition_time", "hours", 4)
-
-
-for (i in nrow(df)) {
-  if (df[i + 1, anid] != df[i, anid] | nrow(df) == i) {
-    df[i, "dt"] <- NA
-  } else if (df[i + 1, anid] == df[i, anid]) {
-    df[i, "dt"] <- difftime(df[i + 1, t], df[i, t], units = unit)
-  }
-}
-for (i in 1:nrow(df)) {
-  if (df[i, "dt"] < low) {
-    j = i
-    while (df[j+1, "dt"] < low | is.na(df[j+1, "dt"])) {
-      df <- df[-(j+1),]
-    }
-  }
-}
-if (df[1, "dt"] < low) {
-  df <- df[!1,]
-}
-df[, "dt"] <- NULL
-
-#########
-bursts <- function(anid, df, t, unit, low) {
-  for (i in 1:nrow(df)) {
-    if (df[i + 1, anid] != df[i, anid] | nrow(df) == i) {
-      df[i, "dt"] <- NA
-    } else if (df[i + 1, anid] == df[i, anid]) {
-      df[i, "dt"] <- difftime(df[i + 1, t], df[i, t], units = unit)
-    }
-  }
-  for (i in 1:nrow(df)) {
-    if (is.na(df[i, "dt"])) {
-    } else if (df[i, "dt"] < low) {
-      j = i
-      while (df[j + 1, "dt"] < low | is.na(df[j+1, "dt"])) {
-        df <- df[-(j+1),]
-      }
-    }
-  }
-  df[, "dt"] <- NULL
-  newfile <- df
-  return(assign("test", newfile, envir = .GlobalEnv))
-}
-
-
-bursts("animal_id", collars, "acquisition_time", "hours", 1)
-
-
+  # Make relevant assignments
+  return(assign(name, newfile, envir = .GlobalEnv))
+} ##############################################################################################################
+################################################################################################################
--- make_steps.R
+++ make_steps.R
@@ -1,46 +1,8 @@
-turn_ang <- function(xlen, ylen, comp =TRUE, as.deg = FALSE) {
-  u = 1
-  if (as.deg) {
-    u = 180/pi
-  }
-  s <- sign(xlen)
-  s[s == 0] <- 1
-  ang = s * (ylen < 0) * pi + atan(xlen / ylen)
-  if (comp) {
-    ang[ang < 0] <- ang[ang < 0] + 2 * pi
-  }
-  return(ang * u)
-}
-
-bearing.ta <- function(xy1, xy2, xy3, as.deg = FALSE) {
-  if (length(xy1) != 2 | length(xy2) != 2 | length(xy3) != 2) {
-    return(NaN)
-  }
-  u = 1
-  if (as.deg) {
-    u = 180 / pi
-  }
-  xylen1 <- xy2 - xy1
-  xylen2 <- xy3 - xy2
-  comp1 <- turn_ang(xylen1[1], xylen1[2], comp = F)
-  comp2 <- turn_ang(xylen2[1], xylen2[2], comp = F)
-  if(is.data.frame(xylen1)) {
-    sl1 <- sqrt(rowSums(xylen1^2))
-    sl2 <- sqrt(rowSums(xylen2^2))
-  } else {
-    sl1 <- sqrt(sum(xylen1^2))
-    sl2 <- sqrt(sum(xylen2^2))
-  }
-  ta = (comp2 - comp1)
-  ta[ta < -pi] = ta[ta < -pi] + 2 * pi
-  ta[ta > pi] = ta[ta > pi] - 2 * pi
-  return(list(comp1 = unlist(comp1 * u), comp2 = unlist(comp2 * u),
-              ta = unlist(ta * u), sl1 = unlist(sl1), sl2 = unlist(sl2)))
-}
-
-
-################# This is a generalized make track function ###################################################################
-track <- function (df, track.name, x, y, bid, as.deg = FALSE, as.comp = FALSE, rm.missing = FALSE, too.few) {
+##############TRACKER###########################################################################################
+################################################################################################################
+tracker <- function (df, track.name, x, y, bid, as.deg = FALSE, as.comp = FALSE, rm.missing = FALSE, too.few) {#
+  ##############################################################################################################
+  ############ Function specifications #########################################################################
   # data is the data being used for the calculation
   # track.name is the name to be specified for the new table that is generated from running this function
   # x is UTM easting
@@ -48,21 +10,26 @@ track <- function (df, track.name, x, y, bid, as.deg = FALSE, as.comp = FALSE, r
   # bid is the column containing burst id numbers
   # as.deg = FALSE is default, keeping the units in radians
   # as.comp = FALSE is default, which keeps the radians on a (+/- pi) scale
-  # rm.missing = FALSE is the default, which keeps all data. rm.missing = TRUE will remove values without turn angles
-  # too.few will specify the fewest number of bursts that the user wants out of the function. For example, too.few = 3
-  # when rm.missing = FALSE would return all bursts with more than three points. The same specification with rm.missing = TRUE
-  # would return all bursts with 3 or more recorded turn angles.
-  ############ Columns to be created ########################
+  # rm.missing = FALSE is the default, which keeps all data. rm.missing = TRUE will remove values without 
+  #   turn angles
+  # too.few will specify the fewest number of bursts that the user wants out of the function. For 
+  #   example, too.few = 3 when rm.missing = FALSE would return all bursts with more than three points. 
+  #   The same specification with rm.missing = TRUE would return all bursts with 3 or more recorded turn angles.
+  ##############################################################################################################
+  ############ Columns to be created ###########################################################################
   # ta is turn angle in radians (+/- pi)
   # sl is step length
   # bear is the bearing, defaulted to radians (+/- pi)
+  ##############################################################################################################
+  ############ Function ########################################################################################
   for (i in 1:nrow(df)) {
     if (i == 1) { 
       df[i, "ta"] <- NA
       df[i, "sl"] <- NA
       df[i, "bear"] <- NA
     } else if (i == 2) { 
-      # If you have only the first two points in a burst (this case second of the table), you can't calculate the turn angle
+      # If you have only the first two points in a burst (this case second of the table), you can't calculate
+      #   the turn angle
       xy2 <- c(df[i, x], df[i, y]) # XY coords of current point
       xy1 <- c(df[i - 1, x], df[i - 1, y]) # XY coords of previous point
       xylen1 <- xy2 - xy1 # Distance between them
@@ -112,7 +79,8 @@ track <- function (df, track.name, x, y, bid, as.deg = FALSE, as.comp = FALSE, r
       s2[s2 == 0] <- 1
       ang2 = s * (xylen2[2] < 0) * pi + atan(xylen2[1] / xylen2[2])
       if(is.data.frame(xylen2)) {
-        sl_ <- sqrt(rowSums(xylen2^2)) # Calculate the current step length (net squared displacement from previous)
+        # Calculate the current step length (net squared displacement from previous)
+        sl_ <- sqrt(rowSums(xylen2^2))
       } else {
         sl_ <- sqrt(sum(xylen2^2))
       }
@@ -123,6 +91,7 @@ track <- function (df, track.name, x, y, bid, as.deg = FALSE, as.comp = FALSE, r
     }
   }
   if (as.comp) {
+    # Corrections for if there are negative pi values and a 2pi scale is desired
     for (i in 1:nrow(df)) {
       if (is.na(df[i, "ta"])) {
       } else if (df[i, "ta"] < 0) {
@@ -134,6 +103,7 @@ track <- function (df, track.name, x, y, bid, as.deg = FALSE, as.comp = FALSE, r
       }
     }
   } else {
+    # Corrections for if a +/- pi scale is desired
     for (i in 1:nrow(df)) {
       if (is.na(df[i, "ta"])) {
       } else if (df[i, "ta"] < -pi) {
@@ -150,22 +120,24 @@ track <- function (df, track.name, x, y, bid, as.deg = FALSE, as.comp = FALSE, r
     }
   }
   if (rm.missing) {
+    # Removes any observations without a turn angle
     df <- df[complete.cases(df[ , "ta"]),]
   }
   u = 1
-  if(as.deg) { # This will convert the angles to degrees if the user so wishes
+  if(as.deg) { 
+    # This will convert the angles to degrees if the user so wishes
     u = 180/pi
     df[, "ta"] <- df[, "ta"] * u
     df[, "bear"] <- df[, "bear"] * u
   }
-  df[, "burst_id"] <- as.factor(df[, "burst_id"])
+  df[, "burst_id"] <- as.factor(df[, "burst_id"]) # Make 'burst_id' a factor
   for (i in levels(df[, "burst_id"])) {
+    # Remove bursts with less than too.few observations
     if (sum(as.numeric(df[, "burst_id"] == i)) < too.few) {
       df <- df[!df[,"burst_id"] == i,]
     }
   }
   newfile <- df
   return(assign(track.name, newfile, envir = .GlobalEnv))
-}
-
-track(test2, "gps_utm_easting", "gps_utm_northing", "burst_id", as.comp = TRUE, rm.missing = TRUE, too.few = 3, track.name = "YES")
+} ##############################################################################################################
+################################################################################################################
